65821c198432f1ba0c77ba13e625514c78f35ab9
utt <meelis.utt@gmail.com>
2025-09-26 12:00:03 +0200

    improvements to info section

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -18,19 +19,27 @@ const (
 	StateCount
 )
 
+var (
+	strToState = map[string]State{
+		"Not Started": NotStarted,
+		"In Progress": InProgress,
+		"Blocked":     Blocked,
+		"Completed":   Completed,
+	}
+	stateToStr = map[State]string{
+		NotStarted: "Not Started",
+		InProgress: "In Progress",
+		Blocked:    "Blocked",
+		Completed:  "Completed",
+	}
+)
+
 func (s State) String() string {
-	switch s {
-	case NotStarted:
-		return "Not Started"
-	case InProgress:
-		return "In Progress"
-	case Blocked:
-		return "Blocked"
-	case Completed:
-		return "Completed"
-	default:
+	val, ok := stateToStr[s]
+	if !ok {
 		return "Undefined"
 	}
+	return val
 }
 
 func StateValues() []State {
@@ -90,6 +99,26 @@ func servePage(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
+func validatePageData(pd PageData) error {
+	zeroTime := time.Time{}
+	if len(pd.MainTask) <= 0 {
+		return fmt.Errorf("main-task is empty")
+	}
+	if len(pd.Responsible) <= 0 {
+		return fmt.Errorf("responsible is empty")
+	}
+	if pd.State < 0 || StateCount <= pd.State {
+		return fmt.Errorf("state is incorrect")
+	}
+	if pd.ParentOrder <= 0 {
+		return fmt.Errorf("parent-order is empty")
+	}
+	if zeroTime.Equal(pd.Deadline) {
+		return fmt.Errorf("deadline is empty")
+	}
+	return nil
+}
+
 func handleSave(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseForm()
@@ -98,38 +127,51 @@ func handleSave(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	formData := PageData{
+		MainTask:    r.FormValue("order-info-main-task"),
+		Responsible: r.FormValue("order-info-responsible"),
+		State:       strToState[r.FormValue("order-info-state")],
+	}
+	parentOrder, _ := strconv.Atoi(r.FormValue("order-info-parent-order"))
+	deadline, _ := time.Parse("2006-01-02", r.FormValue("order-info-deadline"))
+	formData.ParentOrder = parentOrder
+	formData.Deadline = deadline
+
+	if err := validatePageData(formData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("[INFO]: validation failed: %s\n", err)
+		w.Write([]byte(fmt.Sprintf(`{"error": "order-info-state is %s"}`, err)))
+		return
+	}
+
 	changes := []string{}
-	mainTask := r.FormValue("order-info-main-task")
-	if data.MainTask != mainTask && len(mainTask) > 0 {
-		data.MainTask = mainTask
+	if data.MainTask != formData.MainTask {
+		data.MainTask = formData.MainTask
 		changes = append(changes, "main-task")
 	}
 
-	responsible := r.FormValue("order-info-responsible")
-	if data.Responsible != responsible && len(responsible) > 0 {
-		data.Responsible = responsible
+	if data.Responsible != formData.Responsible {
+		data.Responsible = formData.Responsible
 		changes = append(changes, "responsible")
 	}
 
-	parentOrder, err := strconv.Atoi(r.FormValue("order-info-parent-order"))
-	if err == nil && data.ParentOrder != parentOrder && parentOrder > 0 {
-		data.ParentOrder = parentOrder
+	if data.ParentOrder != formData.ParentOrder {
+		data.ParentOrder = formData.ParentOrder
 		changes = append(changes, "parent-order")
 	}
 
-	deadline, err := time.Parse("2006-01-02", r.FormValue("order-info-deadline"))
-	if err == nil && data.Deadline != deadline {
-		data.Deadline = deadline
+	if data.Deadline != formData.Deadline {
+		data.Deadline = formData.Deadline
 		changes = append(changes, "deadline")
 	}
 
-	state, err := strconv.Atoi(r.FormValue("order-info-state"))
-	if err == nil && data.State != State(state) {
-		data.State = State(state)
+	if data.State != formData.State {
+		data.State = formData.State
 		changes = append(changes, "state")
 	}
 
 	if len(changes) > 0 {
 		fmt.Printf("[INFO]: changes to: %v\n", changes)
 	}
+	w.WriteHeader(http.StatusOK)
 }
