64b32b3ae819e8911b16791fb898922372aa143e
utt <meelis.utt@gmail.com>
2025-10-01 17:32:39 +0200

    handlePostUser, tweaks to storage, tracing and spans

--- cmd/user-mgmt/main.go
+++ cmd/user-mgmt/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/http"
 	"runtime"
 	"strconv"
 	"time"
+)
+
+var (
+	Storage storageAPI = nil
+)
+
+type CtxKey struct {
+	key string
+}
+
+var (
+	CtxKeyTrace = CtxKey{key: "trace"}
 )
 
 func randalphanum() string {
@@ -19,10 +32,16 @@ func randalphanum() string {
 	return v
 }
 
-func AddTrace(w http.ResponseWriter) {
-	if len(w.Header().Get("trace")) == 0 {
-		w.Header().Add("trace", randalphanum())
+func AddTrace(ctx context.Context, w http.ResponseWriter) context.Context {
+	trace := w.Header().Get("trace")
+	if len(trace) == 0 {
+		trace = randalphanum()
+		w.Header().Add("trace", trace)
 	}
+	if ctx.Value(CtxKeyTrace) == nil {
+		ctx = context.WithValue(ctx, CtxKeyTrace, trace)
+	}
+	return ctx
 }
 
 func GetTrace(w http.ResponseWriter) string {
@@ -32,73 +51,112 @@ func GetTrace(w http.ResponseWriter) string {
 	return w.Header().Get("trace")
 }
 
-type Spans map[string]*Span
+var Spans map[string][]*Span = make(map[string][]*Span)
 
 type Span struct {
-	FuncName string    `json:"func_name"`
-	Filename string    `json:"filename"`
-	Line     int       `json:"line"`
-	Trace    string    `json:"trace"`
-	Start    time.Time `json:"start"`
-	End      time.Time `json:"end"`
+	FuncName string    `json:"func_name,omitempty"`
+	Filename string    `json:"filename,omitempty"`
+	Line     int       `json:"line,omitempty"`
+	Trace    string    `json:"trace,omitempty"`
+	Start    time.Time `json:"start,omitempty"`
+	End      time.Time `json:"end,omitempty"`
+	Desc     string    `json:"desc,omitempty"`
 }
 
-func (ss Spans) StartSpan(w http.ResponseWriter) {
-	pc, file, line, _ := runtime.Caller(2)
+func PrintSpans(ctx context.Context) {
+	trace := ctx.Value(CtxKeyTrace).(string)
+	spans := Spans[trace]
+	emptyTime := time.Time{}
+	var prevEnd time.Time
+	for _, span := range spans {
+		if span.End.Equal(emptyTime) {
+			if prevEnd.Equal(emptyTime) {
+				span.End = time.Now().UTC()
+			} else {
+				span.End = prevEnd
+			}
+			prevEnd = span.End
+		}
+		bs, err := json.Marshal(span)
+		if err == nil {
+			fmt.Println(string(bs))
+		}
+	}
+}
+
+func StartSpan(ctx context.Context, desc string) {
+	pc, file, line, _ := runtime.Caller(1)
 	fn := runtime.FuncForPC(pc)
-	trace := w.Header().Get("trace")
+	trace := ctx.Value(CtxKeyTrace).(string)
 	s := &Span{
 		FuncName: fn.Name(),
 		Filename: file,
 		Line:     line,
 		Trace:    trace,
 		Start:    time.Now().UTC(),
+		Desc:     desc,
 	}
-	ss[trace] = s
+	Spans[trace] = append(Spans[trace], s)
 }
 
-func (ss Spans) StopSpan(w http.ResponseWriter) {
-	trace := w.Header().Get("trace")
-	s, ok := ss[trace]
+func StopSpan(ctx context.Context, desc string) {
+	pc, file, _, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	trace := ctx.Value(CtxKeyTrace).(string)
+	spans, ok := Spans[trace]
 	if !ok {
 		return
 	}
-	s.End = time.Now().UTC()
+	for _, span := range spans {
+		if span.Filename == file && span.FuncName == fn.Name() && span.Desc == desc {
+			span.End = time.Now().UTC()
+			break
+		}
+	}
 }
 
 type ierror interface {
 	String() string
+	StatusCode() int
+	StatusMessage() string
 }
 
 type erro struct {
-	Code    uint   `json:"code"`
-	Message string `json:"message"`
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 func (e *erro) String() string {
 	return fmt.Sprintf(`{"code": %d, "message": %q}`, e.Code, e.Message)
 }
 
+func (e *erro) StatusCode() int {
+	return e.Code
+}
+func (e *erro) StatusMessage() string {
+	return e.Message
+}
+
 func NewError(code uint, format string, a ...any) ierror {
 	return &erro{
-		Code:    code,
+		Code:    int(code),
 		Message: fmt.Sprintf(format, a...),
 	}
 }
 
 type Meta struct {
-	Version *uint      `json:"version"`
-	Created *time.Time `json:"created"`
-	Updated *time.Time `json:"updated"`
-	Deleted *bool      `json:"deleted"`
+	Version *uint      `json:"version,omitempty"`
+	Created *time.Time `json:"created,omitempty"`
+	Updated *time.Time `json:"updated,omitempty"`
+	Deleted *bool      `json:"deleted,omitempty"`
 }
 
 type User struct {
-	ID         *uint   `json:"id"`
-	Name       *string `json:"name"`
-	Email      *string `json:"email"`
-	Supervisor *string `json:"supervisor"`
-	Meta       *Meta   `json:"meta"`
+	ID         *uint   `json:"id,omitempty"`
+	Name       *string `json:"name,omitempty"`
+	Email      *string `json:"email,omitempty"`
+	Supervisor *string `json:"supervisor,omitempty"`
+	Meta       *Meta   `json:"meta,omitempty"`
 }
 
 type Action int
@@ -111,18 +169,25 @@ const (
 )
 
 type storageAPI interface {
-	Close()
-	Read(ctx context.Context, id int) (*User, ierror)
-	Write(ctx context.Context, action Action, user *User) ierror
+	Close(ctx context.Context)
+	Read(ctx context.Context, id uint) (*User, ierror)
+	Write(ctx context.Context, action Action, user *User) (*User, ierror)
 }
 
 type LocalStorage map[uint][]*User
 
-func (s LocalStorage) Close() {
+func (s LocalStorage) Close(ctx context.Context) {
+
+	StartSpan(ctx, "LocalStorage:Close")
+	defer StopSpan(ctx, "LocalStorage:Close")
+
 	s = nil
 }
 
 func (s LocalStorage) Read(ctx context.Context, id uint) (*User, ierror) {
+	StartSpan(ctx, "LocalStorage:Read")
+	defer StopSpan(ctx, "LocalStorage:Read")
+
 	if s == nil {
 		return nil, NewError(http.StatusInternalServerError, "localstorage not initialized for read")
 	}
@@ -133,28 +198,41 @@ func (s LocalStorage) Read(ctx context.Context, id uint) (*User, ierror) {
 	return us[len(us)-1], nil
 }
 
-func (s LocalStorage) Write(ctx context.Context, action Action, user *User) ierror {
+func (s LocalStorage) Write(ctx context.Context, action Action, user *User) (*User, ierror) {
+
+	StartSpan(ctx, "LocalStorage:Write")
+	defer StopSpan(ctx, "LocalStorage:Write")
+
 	if s == nil {
-		return NewError(http.StatusInternalServerError, "localstorage not initialized for write")
+		return nil, NewError(http.StatusInternalServerError, "localstorage not initialized for write")
 	}
-	if user == nil || user.ID == nil {
-		return NewError(http.StatusInternalServerError, "invalid user object in write")
+	if user == nil {
+		return nil, NewError(http.StatusInternalServerError, "invalid user object in write")
 	}
 
-	us, ok := s[*user.ID]
+	var us []*User
+	var ok bool
+	if user.ID != nil {
+		us, ok = s[*user.ID]
+	}
 
 	switch action {
 
 	case CREATE:
+		StartSpan(ctx, "LocalStorage:Write:CREATE")
 		if ok || len(us) > 0 {
-			return NewError(http.StatusConflict, "already exists during write")
+			return nil, NewError(http.StatusConflict, "already exists during write")
 		}
-		s[*user.ID] = append(s[*user.ID], user)
-		return nil
+		id := uint(len(s) + 1)
+		user.ID = &id
+		s[id] = append(s[id], user)
+		StopSpan(ctx, "LocalStorage:Write:CREATE")
+		return user, nil
 
 	case UPDATE:
+		StartSpan(ctx, "LocalStorage:Write:UPDATE")
 		if !ok || len(us) == 0 {
-			return NewError(http.StatusNotFound, "not found during write")
+			return nil, NewError(http.StatusNotFound, "not found during write")
 		}
 		var updUser User = *(us[len(us)-1])
 		updated := false
@@ -176,29 +254,77 @@ func (s LocalStorage) Write(ctx context.Context, action Action, user *User) ierr
 			*updUser.Meta.Version += 1
 			s[*user.ID] = append(s[*user.ID], &updUser)
 		}
-		return nil
+		us = s[*user.ID]
+		StopSpan(ctx, "LocalStorage:Write:UPDATE")
+		return us[len(us)-1], nil
 
 	case SOFTDELETE:
+		StartSpan(ctx, "LocalStorage:Write:SOFTDELETE")
 		if ok {
 			for _, u := range us {
 				b := true
 				u.Meta.Deleted = &b
 			}
 		}
-		return nil
+		StopSpan(ctx, "LocalStorage:Write:SOFTDELETE")
+		return nil, nil
 
 	case HARDDELETE:
+		StartSpan(ctx, "LocalStorage:Write:HARDDELETE")
 		if ok {
 			delete(s, *user.ID)
 		}
-		return nil
+		StopSpan(ctx, "LocalStorage:Write:HARDDELETE")
+		return nil, nil
 
 	default:
-		return NewError(http.StatusInternalServerError, "undefined write action")
+		return nil, NewError(http.StatusInternalServerError, "undefined write action")
 	}
 }
 
-func handlePostUser(w http.ResponseWriter, r *http.Request) {}
+func handlePostUser(w http.ResponseWriter, r *http.Request) {
+	ctx := AddTrace(context.Background(), w)
+	defer PrintSpans(ctx)
+
+	StartSpan(ctx, "handlePostUser")
+	defer StopSpan(ctx, "handlePostUser")
+
+	var user User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		err := NewError(http.StatusBadRequest, "invalid payload")
+		w.WriteHeader(err.StatusCode())
+		w.Write([]byte(err.String()))
+		return
+	}
+
+	{
+		// validation
+		if user.ID != nil {
+			err := NewError(http.StatusBadRequest, "id not allowed")
+			w.WriteHeader(err.StatusCode())
+			w.Write([]byte(err.String()))
+			return
+		}
+	}
+
+	u, err := Storage.Write(ctx, CREATE, &user)
+	if err != nil {
+		w.WriteHeader(err.StatusCode())
+		w.Write([]byte(err.String()))
+		return
+	}
+
+	bs, jsonerr := json.Marshal(u)
+	if jsonerr != nil {
+		err := NewError(http.StatusInternalServerError, "marshalling user failed")
+		w.WriteHeader(err.StatusCode())
+		w.Write([]byte(err.String()))
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(bs)
+}
 
 func handleGetUserByID(w http.ResponseWriter, r *http.Request) {}
 
@@ -211,6 +337,9 @@ func handlePatchUser(w http.ResponseWriter, r *http.Request) {}
 func handleDeleteUser(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
+
+	Storage = make(LocalStorage)
+
 	http.HandleFunc("POST /user", handlePostUser)
 	http.HandleFunc("GET /user/{id}", handleGetUserByID)
 	http.HandleFunc("GET /users", handleGetUsers)
