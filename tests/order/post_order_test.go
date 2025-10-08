182012f56fb3cd9b2b17e9dca49795b64785f105
utt <meelis.utt@gmail.com>
2025-10-08 17:31:17 +0200

    extend PostOrder cases

--- tests/order/post_order_test.go
+++ tests/order/post_order_test.go
@@ -11,7 +11,80 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func (s *OrderSuite) TestPostOrder() {
+func (s *OrderSuite) TestPostOrder_Minimal() {
+	tt := s.T()
+
+	o := orderObj()
+	zeroOrderIDs(o)
+	o.SetDelegatedTasks(nil)
+	o.SetSitReps(nil)
+	resp, err := s.API.PostOrder(tt, context.Background(), &request.PostOrderRequest{
+		Order: o,
+	})
+	defer cleanup.Order(tt, s.API, resp.GetOrder())
+	require.NoError(tt, err)
+
+	opts := []cmp.Option{
+		compare.IgnoreID,
+	}
+
+	expected := &response.PostOrderResponse{
+		Order: o,
+	}
+
+	compare.RequireEqual(tt, expected, resp, opts...)
+	require.NotEmpty(tt, resp.GetOrder().GetMeta())
+}
+
+func (s *OrderSuite) TestPostOrder_WithDelegatedTasks() {
+	tt := s.T()
+
+	o := orderObj()
+	zeroOrderIDs(o)
+	o.SetSitReps(nil)
+	resp, err := s.API.PostOrder(tt, context.Background(), &request.PostOrderRequest{
+		Order: o,
+	})
+	defer cleanup.Order(tt, s.API, resp.GetOrder())
+	require.NoError(tt, err)
+
+	opts := []cmp.Option{
+		compare.IgnoreID,
+	}
+
+	expected := &response.PostOrderResponse{
+		Order: o,
+	}
+
+	compare.RequireEqual(tt, expected, resp, opts...)
+	require.NotEmpty(tt, resp.GetOrder().GetMeta())
+}
+
+func (s *OrderSuite) TestPostOrder_WithSitReps() {
+	tt := s.T()
+
+	o := orderObj()
+	zeroOrderIDs(o)
+	o.SetSitReps(nil)
+	resp, err := s.API.PostOrder(tt, context.Background(), &request.PostOrderRequest{
+		Order: o,
+	})
+	defer cleanup.Order(tt, s.API, resp.GetOrder())
+	require.NoError(tt, err)
+
+	opts := []cmp.Option{
+		compare.IgnoreID,
+	}
+
+	expected := &response.PostOrderResponse{
+		Order: o,
+	}
+
+	compare.RequireEqual(tt, expected, resp, opts...)
+	require.NotEmpty(tt, resp.GetOrder().GetMeta())
+}
+
+func (s *OrderSuite) TestPostOrder_Full() {
 	tt := s.T()
 
 	o := orderObj()
@@ -31,4 +104,5 @@ func (s *OrderSuite) TestPostOrder() {
 	}
 
 	compare.RequireEqual(tt, expected, resp, opts...)
+	require.NotEmpty(tt, resp.GetOrder().GetMeta())
 }
